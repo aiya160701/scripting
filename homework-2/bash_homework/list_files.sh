df581d651df200373917122a7d98c6309d79636b
aiyalida <aiya160701@gmail.com>
2024-06-13 01:32:26 +0000

    Aiya's homework-2

---
+++ homework-2/bash_homework/list_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FIND=$(cd ~/homework-2/bash_scripting/; echo *)
+
+echo "The contents of bash_scripting are: $FIND"
+
---
+++ homework-2/bash_homework/script_info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "This is my script ID: $$" 
+
+echo "The total amount of arguments passed is: $#"
+
+echo "These are all arguments: $@"
+
+echo "This is the name of the script: $0"
+
+echo "Status code is: $? " 
+
+
+
+
+
